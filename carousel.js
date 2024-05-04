d449bb0101d0c1263ab6eb907f73d23ae85cb692
rosswilliamsdev <rwillguitar@gmail.com>
2024-05-03 21:51:03 -0500

    feat: carousel on topeka page

--- carousel.js
+++ carousel.js
@@ -1,29 +1,24 @@
 let slideIndex = 0;
-const slides = document.querySelectorAll('.slide');
+showSlide(slideIndex);
 
 function showSlide(index) {
-  if (index >= slides.length) {
-    slideIndex = 0;
-  } else if (index < 0) {
-    slideIndex = slides.length - 1;
-  }
-  
-  slides.forEach((slide) => {
-    slide.style.display = 'none';
-  });
-  
-  slides[slideIndex].style.display = 'block';
+    const slides = document.querySelectorAll('.carousel_image');
+    if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    if (index >= slides.length) {
+        slideIndex = 0;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
 }
 
 function nextSlide() {
-  slideIndex++;
-  showSlide(slideIndex);
+    showSlide(++slideIndex);
 }
 
 function prevSlide() {
-  slideIndex--;
-  showSlide(slideIndex);
+    showSlide(--slideIndex);
 }
-
-
-showSlide(slideIndex);
